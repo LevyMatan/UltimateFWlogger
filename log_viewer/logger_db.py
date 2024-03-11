e9354db8b100c297e4358602168af0c4d8236841
Matan Levy <levymatanlevy@gmail.com>
2024-03-11 04:51:31 +0200

    Add log timestamp, file, line, and message attributes to Log class

--- log_viewer/logger_db.py
+++ log_viewer/logger_db.py
@@ -4,16 +4,30 @@ from sqlalchemy import inspect
 from sqlalchemy.ext.declarative import declarative_base
 import threading
 import datetime
+import random
 
 Base = declarative_base()
 
 class Log(Base):
+    """
+    Represents a log entry in the database.
+
+    Attributes:
+        id (int): The unique identifier of the log entry.
+        timestamp (str): The timestamp of when the log entry was created.
+        file (str): The file name associated with the log entry.
+        line (int): The line number in the file where the log entry occurred.
+        level (str): The log level of the entry (e.g., INFO, WARNING, ERROR).
+        msg (str): The log message.
+    """
     __tablename__ = 'logs'
 
     id = Column(Integer, Sequence('log_id_seq'), primary_key=True)
-    date = Column(String)
+    timestamp = Column(Integer)
+    file = Column(String)
+    line = Column(Integer)
     level = Column(String)
-    log = Column(String)
+    msg = Column(String)
 
 
 class DatabaseThread(threading.Thread):
@@ -30,30 +44,56 @@ class DatabaseThread(threading.Thread):
         if 'logs' in table_names:
             self.log_id = 0
             if self.session.query(Log).count() > 0:
-                self.log_id = self.session.query(Log).order_by(Log.id.desc()).first().id        
+                self.log_id = self.session.query(Log).order_by(Log.id.desc()).first().id
                 self.log_id += 1
         self.last_read_id = 0
 
-    def insert_log(self, date, level, log):
-        new_log = Log(id=self.log_id, date=date, level=level, log=log)
+    def insert_log(self, timestamp, file, line, level, msg):
+        """
+        Inserts a new log entry into the database.
+
+        Args:
+            date (datetime): The date and time of the log entry.
+            level (str): The log level (e.g., INFO, WARNING, ERROR).
+            msg (str): The log message.
+
+        Returns:
+            None
+        """
+        new_log = Log(id=self.log_id, timestamp=timestamp, file=file, line=line, level=level, msg=msg)
         self.session.add(new_log)
         self.session.commit()
         self.log_id += 1
 
     def get_all_logs(self):
-        return self.session.query(Log).all()
+            """
+            Retrieve all logs from the database.
+
+            Returns:
+                A list of Log objects representing all the logs in the database.
+            """
+            return self.session.query(Log).all()
 
     def get_new_logs(self):
-        if self.last_read_id is None:
-            logs = self.session.query(Log).all()
-        else:
-            logs = self.session.query(Log).filter(Log.id > self.last_read_id).all()
+            """
+            Retrieves new logs from the database.
 
-        if logs:
-            self.last_read_id = logs[-1].id
+            If self.last_read_id is None, retrieves all logs.
+            Otherwise, retrieves logs with an id greater than self.last_read_id.
 
-        return logs
-    
+            Returns:
+                A list of Log objects representing the new logs.
+            """
+            if self.last_read_id is None:
+                logs = self.session.query(Log).all()
+            else:
+                logs = self.session.query(Log).filter(Log.id > self.last_read_id).all()
+
+            if logs:
+                self.last_read_id = logs[-1].id
+
+            return logs
+
     def close(self):
         self.session.close()
 
@@ -62,13 +102,14 @@ if __name__ == '__main__':
     db_thread = DatabaseThread('logs.db')
     db_thread.start()
     db_thread.join()
-    # logs = db_thread.get_all_logs()
-    # for log in logs:
-    #     print(log.id, log.date, log.level, log.log)
-    db_thread.insert_log(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'INFO', 'This is an info message')
-    db_thread.insert_log(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'ERROR', 'This is an error message')
-    db_thread.insert_log(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'WARNING', 'This is a warning message')
+    db_thread.insert_log(
+        timestamp=datetime.datetime.now(),
+        file='fileA.c',
+        line=random.randint(1,1000),
+        level='INFO',
+        msg='This is a test log message'
+    )
     logs = db_thread.get_new_logs()
     for log in logs:
-        print(log.id, log.date, log.level, log.log)
+        print(log.id, log.timestamp, log.level, log.msg)
     db_thread.close()
--- log_viewer/sample_logs_generator.py
+++ log_viewer/sample_logs_generator.py
@@ -13,18 +13,40 @@ import datetime
 from logger_db import DatabaseThread
 
 class LogGenerator:
+    """
+    LogGenerator class is responsible for generating and writing logs.
+
+    Attributes:
+        db_thread (Thread): The thread used for database operations.
+        levels (list): The list of log levels.
+        files (list): The list of file names.
+
+    Methods:
+        generate_logs(): Generates a list of logs.
+        write_logs_to_db(): Writes logs to the database.
+        write_to_shared_log_file(): Writes logs to a shared log file.
+    """
+
     def __init__(self, db_thread):
         self.db_thread = db_thread
         self.levels = ['INFO', 'WARNING', 'ERROR']
-
+        self.files = ['fileA.c', 'fileB.c', 'fileC.c']
 
     def generate_logs(self):
+        """
+        Generates a list of logs.
+
+        Returns:
+            list: A list of logs, where each log is a tuple containing the log information.
+        """
         logs = [tuple] * 100
         for i in range(100):
-            date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            date = datetime.datetime.now()
             level = random.choice(self.levels)
-            message = f'This is a {level} message'
-            logs[i] = (date, level, message)
+            file = random.choice(self.files)
+            line = random.randint(1, 1000)
+            message = f'This is a {level} message from {file} at {date}'
+            logs[i] = (date, file, line, level, message)
         return logs
 
     def write_logs_to_db(self):
@@ -39,7 +61,7 @@ class LogGenerator:
         """
         self.db_thread.start()
         for log in self.generate_logs():
-            self.db_thread.insert_log(log[0], log[1], log[2])
+            self.db_thread.insert_log(log[0], log[1], log[2], log[3], log[4])
             time.sleep(1)
         self.db_thread.close()
 
@@ -59,7 +81,7 @@ class LogGenerator:
         """
         for log in self.generate_logs():
             with open('shared_log_file.txt', 'a', encoding='utf-8') as f:
-                f.write(f'{log[0]},{log[1]},{log[2]}\n')
+                f.write(f'{log[0]},{log[1]},{log[2]},{log[3]},{log[4]}\n')
             time.sleep(1)
 
 
