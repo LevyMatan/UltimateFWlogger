ca03c84029234d959e4eb9fb716355d630efaa47
Matan Levy <levymatanlevy@gmail.com>
2024-03-11 04:51:09 +0200

    Add web browser functionality to open log viewer in a new tab

--- log_viewer/app.py
+++ log_viewer/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from logger_db import DatabaseThread
-from logger_db import Log
+import webbrowser
 
 app = Flask(__name__)
 
@@ -15,10 +15,18 @@ def index():
 
 @app.route('/logs', methods=['GET'])
 def get_logs():
+    """
+    Retrieve all logs from the database and render them in the log_viewer.html template.
+
+    Returns:
+        The rendered template with the logs.
+    """
     logs = db_thread.get_all_logs()
     return render_template('log_viewer.html', logs=logs)
 
 
 if __name__ == '__main__':
+    url = "http://localhost:8080/"
+    webbrowser.open(url, new=2)  # open in new tab, if possible
     app.run(debug=True, port=8080)
     db_thread.close()
